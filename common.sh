ca1c22c7bac2b354a91800a265a30504eca156fa
jvenegas <jota.montecino@gmail.com>
2018-01-29 14:46:50 +0100

    ADD/ ajout des alias common et des alias git

---
+++ common.sh
@@ -0,0 +1,11 @@
+# Déplacement
+
+alias ..='cd ../'
+alias ...='cd ../..'
+
+alias Projets='cd ~/Projets'
+
+# Clear sh
+ alias cls='printf "\033c"'
+
+
---
+++ git_aliases.sh
@@ -0,0 +1,3 @@
+alias gc='git commit -m $1'
+alias ga='git add $*'
+alias gs='git status'
