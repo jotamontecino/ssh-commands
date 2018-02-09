0ee9fa0f9333b2d8b221fa3fbe4e67e5595f6e42
jvenegas <jota.montecino@gmail.com>
2018-02-09 10:41:52 +0100

    MOD: ajouot du PS1

--- common.sh
+++ common.sh
@@ -4,8 +4,15 @@ alias ..='cd ../'
 alias ...='cd ../..'
 
 alias Projets='cd ~/Projets'
+alias p:='cd ~/Projets'
 
+alias conf='cd ~/.bash_aliases'
 # Clear sh
  alias cls='printf "\033c"'
 
 
+## Bin up and down to research through history
+bind '"\e[A": history-search-backward'
+bind '"\e[B": history-search-forward'
+bind '"\eOA": history-search-backward'
+bind '"\eOB": history-search-forward'
--- git_aliases.sh
+++ git_aliases.sh
@@ -1,3 +1,14 @@
-alias gc='git commit -m $1'
-alias ga='git add $*'
-alias gs='git status'
+alias gci='git commit -m $1'
+alias gad='git add $*'
+alias gst='git status'
+
+# Beaux logs en liste
+alias gls='git log --pretty=format:"%C(yellow)%h%Cred%d\\ %Creset%s%Cblue\\ [%cn]" --decorate'
+
+# Logs en liste verbeux : nombre de mods
+alias gll='git log --pretty=format:"%C(yellow)%h%Cred%d\\ %Creset%s%Cblue\\ [%cn]" --decorate --numstat'
+
+alias glnc='git log --pretty=format:"%h\\ %s\\ [%cn]"'
+
+export PS1="\[\033[36m\]\u\[\033[m\]@\e[31mRed\h:\[\033[32m\] \[\033[33;1m\]\w\[\033[m\] (\$(git branch 2>/dev/null | grep '^*' | colrm 1 2)) \$  \n| => "
+export PS2="| => "
